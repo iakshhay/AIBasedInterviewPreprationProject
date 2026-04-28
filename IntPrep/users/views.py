1b9f015fd40e109c70e53686f57ac438aff9b0c0
AKSHAY PATIL <akshaypatil9280@gmail.com>
2026-04-29 02:12:27 +0530

    Implement authentication system: add user views, JWT login, and custom UserManager

--- IntPrep/users/views.py
+++ IntPrep/users/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .serializers import RegisterSerializer,LoginSerializer,UserSerializer
 
 # Create your views here.
+class RegistrationView(GenericAPIView):
+    queryset=User.objects.all()
+    serializer_class=RegisterSerializer
+
+    def post(self,request):
+        serializer=self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                                "message":"User registered successfully",
+                                "data":serializer.data
+                            },
+                            status=status.HTTP_201_CREATED)
+        return Response({
+                "message":"Registration failed",
+                "errors":serializer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(GenericAPIView):
+    serializer_class=LoginSerializer
+
+    def post(self,request):
+        serailizer = self.get_serializer(data=request.data)
+
+        if serailizer.is_valid():
+            user = serailizer.validated_data['user']
+
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                "message":"Login successfull",
+                "data":{
+                    "user":UserSerializer(user).data,
+                    "access":str(refresh.access_token),
+                    "refres":str(refresh)
+                }
+            },
+            status=status.HTTP_200_OK)
+        
+        return Response(
+            {
+                "message":"Login failed",
+                "errors": serailizer.errors
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
